dcb7fc3e23ce240324826f85a921df9d3661a8ad
marcelogalacci <mrcgalacci@gmail.com>
2020-01-28 17:03:46 -0300

    Arquivo de comunicação com BD

--- app/src/main/java/com/projeto_integrador_iii/MainActivity.java
+++ app/src/main/java/com/projeto_integrador_iii/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     private Button bTVisualizar;
     private ImageButton ibtnCadastrar;
 
+    //
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
