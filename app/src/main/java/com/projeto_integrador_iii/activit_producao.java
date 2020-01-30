cf53c3f3ff5817efa64113117d4e23edd4664941
marcelogalacci <mrcgalacci@gmail.com>
2020-01-30 13:57:15 -0300

    Arquivo de comunicação com BD

---
+++ app/src/main/java/com/projeto_integrador_iii/activit_producao.java
@@ -0,0 +1,20 @@
+package com.projeto_integrador_iii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class activit_producao extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activit_producao);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
